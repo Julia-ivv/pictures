646714067f91d2a65623630ce43d879043827336
Julia Ivanova <julia_sji85@mail.ru>
2020-05-27 18:43:04 +0300

    env

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -17,8 +19,13 @@ module.exports = {
                 exclude: /node_modules/ 
             },
             {
-                test: /\.css$/, 
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] 
+                test: /\.css$/i, 
+                // use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] 
+                use: [
+                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                    'css-loader', 
+                    'postcss-loader'
+                ]
             },
             {
                 test: /\.(gif|ico|png|jpe?g|svg)$/i,
@@ -33,13 +40,22 @@ module.exports = {
         ]
     },
     plugins: [ 
-       new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}), //'style.css'}),
-       new HtmlWebpackPlugin({
-        inject: false, 
-        hash: true, // для страницы нужно считать хеш  Удалить???
-        template: './src/index.html', 
-        filename: 'index.html' 
-      }),
-      new WebpackMd5Hash()
+        new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}), //'style.css'}),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                    preset: ['default'],
+            },
+            canPrint: true
+        }),
+        new HtmlWebpackPlugin({
+            inject: false, 
+            hash: true, // для страницы нужно считать хеш  Удалить???
+            template: './src/index.html', 
+            filename: 'index.html' 
+        }),
+        new WebpackMd5Hash(),
+        new webpack.DefinePlugin({'NODE_ENV': JSON.stringify(process.env.NODE_ENV)})
     ]
 }
