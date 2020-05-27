a9cdf5f447f6a816ba50b7a74c6b3fe60bfb032b
Julia Ivanova <julia_sji85@mail.ru>
2020-05-27 17:34:52 +0300

    hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        // filename: 'main.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [{ 
@@ -20,6 +23,13 @@ module.exports = {
         ]
     },
     plugins: [ 
-       new MiniCssExtractPlugin({filename: 'style.css'})
+       new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}), //'style.css'}),
+       new HtmlWebpackPlugin({
+        inject: false, 
+        hash: true, // для страницы нужно считать хеш  Удалить???
+        template: './src/index.html', 
+        filename: 'index.html' 
+      }),
+      new WebpackMd5Hash()
     ]
 }
